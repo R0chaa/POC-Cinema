1daf3f41ec71b520e7d44d5e1817146ee0a4d0d9
lucas rocha <73042947+R0chaa@users.noreply.github.com>
2024-11-21 21:11:34 +0000

    feat: add json file

--- poc-6/src/app/page.js
+++ poc-6/src/app/page.js
@@ -1,31 +1,26 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Sala from "./components/Sala/Sala";
 import Infos from "./components/Infos/Infos";
 import styles from "./page.module.css";
+import dados from "./dados.json";
 
 const Home = () => {
-  const movieName = "Agropesca Jacaré";
-  const sessionTime = "19:00";
+  const [assentos, setAssentos] = useState([]);
 
-  const sinopse =
-  "Um ano depois de encerrar o ensino médio, o jovem Isaías Wright não tem planos para o futuro e é desafiado por sua mãe solo e um empresário de sucesso a começar a traçar um rumo melhor para sua vida. Ele passa a ser discipulado pelo seu novo mentor, conta com orações de sua mãe e de uma guerreira de orações, Dona Clara, e começa a descobrir o propósito de Deus para sua vida.";
-
-  const data = "26 de setembro de 2024 (Brasil)";
-  const dir = "Alex Kendrick";
-
-  const [assentos, setAssentos] = useState(
-    Array.from({ length: 8 }, () =>
-      Array.from({ length: 8 }, () => "disponivel")
-    )
-  );
+  useEffect(() => {
+    const formattedAssentos = Array.from({ length: 8 }, (_, rowIndex) =>
+      Array.from({ length: 8 }, (_, colIndex) => {
+        const numero = rowIndex * 8 + colIndex + 1;
+        const assento = dados.assentos.find(a => a.numero === numero);
+        return assento ? (assento.disponivel ? "disponivel" : "indisponivel") : "indisponivel";
+      })
+    );
+    setAssentos(formattedAssentos);
+  }, []);
 
   const handleComprar = () => {
-    const temSelecionado = assentos.some(row =>
-      row.includes("selecionado")
-    );
-
-    if (!temSelecionado) {
+    if (!assentos.some(row => row.includes("selecionado"))) {
       alert("Selecione pelo menos um assento antes de comprar.");
       return;
     }
@@ -38,11 +33,11 @@ const Home = () => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.movieTitle}>{movieName}</h1>
-      <h2 className={styles.sessionTime}>Horário: {sessionTime}</h2>
+      <h1 className={styles.movieTitle}>{dados.titulo}</h1>
+      <h2 className={styles.sessionTime}>Horário: {dados.horario}</h2>
       <div className={styles.content}>
         <Sala assentos={assentos} setAssentos={setAssentos} />
-        <Infos sinopse={sinopse} data={data} dir={dir} />
+        <Infos sinopse={dados.sinopse} data={dados.dataLancamento} dir={dados.direcao} />
       </div>
       <button onClick={handleComprar} className={styles.buyButton}>
         Comprar
