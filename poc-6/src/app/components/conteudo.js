b24721abe001aa757452cda4ba9c22ccb0d1116d
Leonardo Pinheiro de Souza <65033540+Lpins01@users.noreply.github.com>
2024-11-21 14:48:18 +0000

    Adicionando informações principais

--- poc-6/src/app/components/conteudo.js
+++ poc-6/src/app/components/conteudo.js
@@ -2,13 +2,22 @@ import styles from "./conteudo.module.css"
 
 export default function Conteudo({dados}) {
     return (
-        <div>
-            <p className={styles.titulo}>Sinopse do Filme:</p>
-            <p className={styles.texto}>{dados.sinopse}</p>
-            <p className={styles.titulo}>Data de Lançamento:</p>
-            <p className={styles.texto}>{dados.dataLancamento}</p>
-            <p className={styles.titulo}>Direção:</p>
-            <p className={styles.texto}>{dados.direcao}</p>
+        <div className={styles.div_container}>
+            <div>
+                {dados.assentos.map((dado) => (
+                    <li key={dado.numero}>
+                    {dado.numero}: {dado.disponivel}
+                    </li>
+                ))}
+            </div>
+            <div>
+                <p className={styles.titulo}>Sinopse do Filme:</p>
+                <p className={styles.texto}>{dados.sinopse}</p>
+                <p className={styles.titulo}>Data de Lançamento:</p>
+                <p className={styles.texto}>{dados.dataLancamento}</p>
+                <p className={styles.titulo}>Direção:</p>
+                <p className={styles.texto}>{dados.direcao}</p>
+            </div>
         </div>
     )
 }
