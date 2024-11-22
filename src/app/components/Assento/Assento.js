97044a203b3372d34ddeb1b1540636b4765b87fb
lucas rocha <73042947+R0chaa@users.noreply.github.com>
2024-11-22 21:33:29 +0000

    feat: add Assento component with styles for seat availability

---
+++ src/app/components/Assento/Assento.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Assento.module.css";
+
+const Assento = ({ estado, onClick, lightMode }) => {
+  const className = `${styles.seat} ${
+    lightMode ? styles[`${estado}Light`] : styles[estado]
+  }`;
+
+  return <div className={className} onClick={estado === "indisponivel" ? null : onClick}></div>;
+};
+
+export default Assento;
