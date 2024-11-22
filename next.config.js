d92baa6fac2b8d3f4a5e8e0e58a024ccbaa639fe
lucas rocha <73042947+R0chaa@users.noreply.github.com>
2024-11-22 19:00:29 -0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Gera arquivos estáticos
-  basePath: '/NOME-DO-REPOSITORIO', // Substitua pelo nome do repositório
+  basePath: '/POC-Cinema', 
   images: {
-    unoptimized: true, // Desativa a otimização de imagens no Next.js
+    unoptimized: true, 
   },
 };
 
